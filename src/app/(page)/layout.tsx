a3c64bc71ba6b7bc58fa811eafc23ed5cd3c4832
王石川 <gavin@wangshichuandeMac-mini.local>
2024-07-12 21:50:53 +0800

    added: Suspense

--- src/app/(page)/layout.tsx
+++ src/app/(page)/layout.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useMemo } from 'react'
+import React, { Suspense, useMemo } from 'react'
 import LeftSideBar from '@/component/SideBar'
 import { ModelSelect } from '@/component/PageHeader'
 import IconEdit from '@/assets/icons/icon-edit.svg'
@@ -10,6 +10,7 @@ import { SnackbarProvider } from 'notistack';
 import MobileIconMenu from '@/component/ Drawer'
 import { CHAT_MODEL_CONVERTER } from '@/interface/common'
 import { usePathname } from 'next/navigation'
+import { Typography } from 'antd'
 
 export default function PageLayout({ children }: { children: React.ReactNode }) {
     const asPathName = usePathname()
@@ -33,28 +34,30 @@ export default function PageLayout({ children }: { children: React.ReactNode }) 
 
 
     return (
-        <RecoilRoot>
-            <Updated />
-            <div className='h-screen w-full overflow-hidden flex relative z-0'>
-                <div className='flex-shrink-0 overflow-x-hidden bg-[#f9f9f9] hidden md:block'>
-                    <LeftSideBar />
-                </div>
-                <div className='relative flex-1 flex flex-col h-full max-w-full overflow-hidden'>
-                    <div className='w-full min-h-[40px] sticky top-0 left-0 flex items-center justify-center border-b border-token-border-medium md:hidden'>
-                        <MobileIconMenu />
-                        {modelMenuList.length > 0 && <ModelSelect modeList={modelMenuList} />}
-                        {!isGpts && <button className='absolute bottom-0 right-0 top-0 flex items-center'>
-                            <span className='px-3'>
-                                <IconEdit />
-                            </span>
-                        </button>}
+        <Suspense fallback={<Typography>error</Typography>}>
+            <RecoilRoot>
+                <Updated />
+                <div className='h-screen w-full overflow-hidden flex relative z-0'>
+                    <div className='flex-shrink-0 overflow-x-hidden bg-[#f9f9f9] hidden md:block'>
+                        <LeftSideBar />
                     </div>
-                    <main className='relative h-full w-full flex-1 overflow-auto transition-width'>
-                        {children}
-                    </main>
+                    <div className='relative flex-1 flex flex-col h-full max-w-full overflow-hidden'>
+                        <div className='w-full min-h-[40px] sticky top-0 left-0 flex items-center justify-center border-b border-token-border-medium md:hidden'>
+                            <MobileIconMenu />
+                            {modelMenuList.length > 0 && <ModelSelect modeList={modelMenuList} />}
+                            {!isGpts && <button className='absolute bottom-0 right-0 top-0 flex items-center'>
+                                <span className='px-3'>
+                                    <IconEdit />
+                                </span>
+                            </button>}
+                        </div>
+                        <main className='relative h-full w-full flex-1 overflow-auto transition-width'>
+                            {children}
+                        </main>
+                    </div>
                 </div>
-            </div>
-            <SnackbarProvider maxSnack={1} />
-        </RecoilRoot>
+                <SnackbarProvider maxSnack={1} />
+            </RecoilRoot>
+        </Suspense>
     )
 }
